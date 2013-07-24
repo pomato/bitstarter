8a16fa12a789365759258773103925af12c57069
PoMaTo <videsp@hotmail.com>
2013-07-23 22:57:17 -0400

    read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = new Buffer(fs.readFileSync('index.html'));
+  //response.send('Hello World2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
